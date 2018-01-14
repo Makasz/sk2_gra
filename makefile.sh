30375267e30dcb950231b5d2ce840b550ba5cca9
PhilBug <filip.bugaj13@gmail.com>
2018-01-14 21:57:21 +0100

    rm s if already exists

--- makefile.sh
+++ makefile.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ -f s ]; then
+    rm s
+	echo "removed previous server executable"
+fi
 g++-5 server_new.cpp -Wall -std=c++11 -o s
 echo "compilation succesfull, the server will now start"
 echo ""
